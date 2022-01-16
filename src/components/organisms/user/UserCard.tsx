177dcc1f2ea3bb52fd6ff784cc6215d56b28c292
azhimattsu <90542867+azhimattsu@users.noreply.github.com>
2022-01-16 14:21:26 +0900

    ユーザー一覧画面の作成

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,37 @@
+import { memo, VFC } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      w="200px"
+      h="200px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="100px"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="large" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm">{fullName}</Text>
+      </Stack>
+    </Box>
+  );
+});
